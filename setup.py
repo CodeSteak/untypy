10a06a5bdcd5abc28c85911f50ad15a2e3f64743
CodeSteak <codesteak@shellf.art>
2021-03-20 17:57:42 +0100

    add setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
-print("TODO: SETUP.PY!")
-raise Exception("unimplemented")
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='untypy',
+      version='0.0.1',
+      description='Dynamic Type Checking For Python',
+      author='Codesteak',
+      author_email='Codesteak@shellf.art',
+      url='https://github.com/CodeSteak/untypy',
+      packages=[],
+      )
