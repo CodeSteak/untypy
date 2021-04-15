98304b11dd01ebd42abd48b016aa934f2a5d86b6
CodeSteak <codesteak@shellf.art>
2021-04-15 17:09:12 +0200

    Implement List Decorator for Type Checking

--- untypy/impl/list.py
+++ untypy/impl/list.py
@@ -30,14 +30,7 @@ class ListChecker(TypeChecker):
         if not issubclass(type(arg), list):
             raise ctx.wrap(UntypyTypeError(arg, self.describe()))
 
-        # TODO: Identity Crises
-
-        #new_list = []
-        for item in arg:
-            self.inner.check_and_wrap(item, ListExecutionContext(ctx))
-            #new_list.append(res)
-
-        return arg
+        return TypedList(arg, self.inner, ctx)
 
     def describe(self) -> str:
         return f"list[{self.inner.describe()}]"
@@ -46,15 +39,152 @@ class ListChecker(TypeChecker):
 class ListExecutionContext(ExecutionContext):
     upper: ExecutionContext
 
-    def __init__(self, upper : ExecutionContext):
+    def __init__(self, upper: ExecutionContext):
         self.upper = upper
 
     def wrap(self, err: UntypyTypeError) -> UntypyTypeError:
         err = err.with_frame(Frame(
-                f"list[{err.expected}]",
-                (" "*len("list[") + err.expected_indicator),
-                None,
-                None,
-                None
-            ))
+            f"list[{err.expected}]",
+            (" " * len("list[") + err.expected_indicator),
+            None,
+            None,
+            None
+        ))
         return self.upper.wrap(err)
+
+
+class TypedList(list):
+    inner: list
+    checker: TypeChecker
+    ctx: ExecutionContext
+
+    def __init__(self, lst, checker, ctx):
+        super().__init__()
+        self.checker = checker
+        self.inner = lst
+        self.ctx = ctx
+
+    # Perform type check
+    def __getitem__(self, index):
+        if type(index) is int:
+            # list[1], flat get
+            ret = self.inner.__getitem__(index)
+            return self.checker.check_and_wrap(ret, self.ctx)
+        else:
+            # returned structure is an list itself.
+            # e.g. list[1:3, ...]
+            return list(map(lambda x: self.checker.check(x, self.ctx), self.inner.__getitem__(index)))
+
+    def __iter__(self):
+        return TypedListIterator(self)
+
+    def append(self, x) -> None:
+        self.inner.append(self.checker.check_and_wrap(x, self.ctx))
+
+    def extend(self, iterable) -> None:
+        return self.inner.extend(list(map(lambda x: self.checker.check_and_wrap(x, self.ctx), iterable)))
+
+    def insert(self, index, obj) -> None:
+        return self.inner.insert(index, self.checker.check_and_wrap(obj, self.ctx))
+
+    def __iadd__(self, other):
+        self.extend(other)
+        return self
+
+    def __setitem__(self, *args, **kwargs):
+        return self.inner.__setitem__(*args, **kwargs)
+
+    def __add__(self, *args, **kwargs):
+        return self.inner.__add__(*args, **kwargs)
+
+    def pop(self, *args, **kwargs):
+        ret = self.inner.pop(*args, **kwargs)
+        return self.checker.check_and_wrap(ret, self.ctx)
+
+    # Delete, Copy, ...
+    def index(self, *args, **kwargs):
+        return self.inner.index(*args, **kwargs)
+
+    def clear(self, *args, **kwargs):
+        return self.inner.clear(*args, **kwargs)
+
+    def copy(self, *args, **kwargs):
+        return self.inner.copy(*args, **kwargs)
+
+    def count(self, *args, **kwargs):
+        return self.inner.count(*args, **kwargs)
+
+    def remove(self, *args, **kwargs):
+        return self.inner.remove(*args, **kwargs)
+
+    def reverse(self, *args, **kwargs):
+        return self.inner.reverse(*args, **kwargs)
+
+    def sort(self, *args, **kwargs):
+        return self.inner.sort(*args, **kwargs)
+
+    def __class_getitem__(self, *args, **kwargs):
+        return self.inner.__class_getitem__(*args, **kwargs)
+
+    def __contains__(self, *args, **kwargs):
+        return self.inner.__contains__(*args, **kwargs)
+
+    def __delitem__(self, *args, **kwargs):
+        return self.inner.__delitem__(*args, **kwargs)
+
+    def __eq__(self, *args, **kwargs):
+        return self.inner.__eq__(*args, **kwargs)
+
+    def __ge__(self, *args, **kwargs):
+        return self.inner.__ge__(*args, **kwargs)
+
+    def __gt__(self, *args, **kwargs):
+        return self.inner.__gt__(*args, **kwargs)
+
+    def __imul__(self, *args, **kwargs):
+        return self.inner.__imul__(*args, **kwargs)
+
+    def __len__(self, *args, **kwargs):
+        return self.inner.__len__(*args, **kwargs)
+
+    def __le__(self, *args, **kwargs):
+        return self.inner.__le__(*args, **kwargs)
+
+    def __lt__(self, *args, **kwargs):
+        return self.inner.__lt__(*args, **kwargs)
+
+    def __mul__(self, *args, **kwargs):
+        return self.inner.__mul__(*args, **kwargs)
+
+    def __ne__(self, *args, **kwargs):
+        return self.inner.__ne__(*args, **kwargs)
+
+    def __repr__(self, *args, **kwargs):
+        return self.inner.__repr__(*args, **kwargs)
+
+    def __reversed__(self, *args, **kwargs):
+        return self.inner.__reversed__(*args, **kwargs)
+
+    def __rmul__(self, *args, **kwargs):
+        return self.inner.__rmul__(*args, **kwargs)
+
+    def __sizeof__(self, *args, **kwargs):
+        return self.inner.__sizeof__(*args, **kwargs)
+
+
+class TypedListIterator:
+    inner: TypedList
+    index: int
+
+    def __init__(self, inner):
+        super().__init__()
+        self.inner = inner
+        self.index = 0
+
+    def __next__(self):
+        if self.index >= len(self.inner):
+            raise StopIteration
+
+        ret = self.inner[self.index]
+        self.index += 1
+        return ret
