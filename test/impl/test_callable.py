da18937e0b8ef993d1c13e02d4f3974ea09b65e3
CodeSteak <codesteak@shellf.art>
2021-04-23 17:08:54 +0200

    add Unittest for Callable

--- test/impl/test_callable.py
+++ test/impl/test_callable.py
@@ -12,9 +12,9 @@ class TestCallable(unittest.TestCase):
     fn2: Callable
 
     def setUp(self) -> None:
-        checker = CallableFactory().create_from(Callable[[int, int], str], DefaultCreationContext())
-        self.fn1 = checker.check_and_wrap(lambda x, y: str(x // y), DummyExecutionContext())
-        self.fn2 = checker.check_and_wrap(lambda x, y: x // y, DummyExecutionContext())
+        self.checker = CallableFactory().create_from(Callable[[int, int], str], DefaultCreationContext())
+        self.fn1 = self.checker.check_and_wrap(lambda x, y: str(x // y), DummyExecutionContext())
+        self.fn2 = self.checker.check_and_wrap(lambda x, y: x // y, DummyExecutionContext())
 
     def test_normal(self):
         self.assertEqual(self.fn1(100, 20), "5")
@@ -46,4 +46,16 @@ class TestCallable(unittest.TestCase):
         self.assertEqual(i, "                     ^^^")
 
         # This DummyExecutionContext is responsable
+        self.assertEqual(cm.exception.frames[-1].file, "dummy")
+
+    def test_not_a_callable(self):
+        with self.assertRaises(UntypyTypeError) as cm:
+            self.checker.check_and_wrap("Hello", DummyExecutionContext())
+
+        (t, i) = cm.exception.next_type_and_indicator()
+        i = i.rstrip()
+
+        self.assertEqual(t, "Callable[[int, int], str]")
+        self.assertEqual(i, "^^^^^^^^^^^^^^^^^^^^^^^^^")
+
         self.assertEqual(cm.exception.frames[-1].file, "dummy")
